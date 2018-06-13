3d0bc23b2a715bdd1a71e184b834fca24303cc00
Drobniac <drobniac@gmail.com>
2018-06-13 11:27:52 +0200

    Update start script

--- start.js
+++ start.js
@@ -1,15 +1,18 @@
 const { exec } = require('child_process');
 
-let outputStdout = (_, stdout, __) => {
-    console.log(stdout);
+let execOutputFunc = (err, stdout, stderr) => {
+    console.log(`Error happened: ${err.message}`);
+    console.log('Stdout: ' + stdout);
+    console.log('Stderr: ' + stderr);
 }
 
 var OStoScriptComposite = {
     Windows: {
         isCurrentOS: 'win32' === process.platform,
         action: () => {
-            exec('docker-compose up', outputStdout);
-            exec('ng serve', outputStdout);
+            exec('docker-compose up', execOutputFunc);
+            exec('npm test', execOutputFunc);
+            exec('npm run-script serve', execOutputFunc);
         }
     }
 };
