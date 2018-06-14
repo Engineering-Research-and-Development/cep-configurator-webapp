58ca46a8e10f626432b2974eb4e2b6118d18d3af
Vlada <vlada@bed.coma>
2018-06-14 11:17:12 +0200

    Add engine-details component

---
+++ src/app/engine-details/engine-details.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { RuleService } from '../rule.service';
+import { identifierModuleUrl } from '@angular/compiler';
+import { EnginesComponent } from '../engines/engines.component';
+
+@Component({
+  selector: 'app-engine-details',
+  templateUrl: './engine-details.component.html',
+  styleUrls: ['./engine-details.component.css']
+})
+export class EngineDetailsComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private ruleService: RuleService,
+    private location: Location
+  ) {}
+
+  engine: any;
+
+  ngOnInit(): void {
+    this.getEngineDetails();
+  }
+
+  getEngineDetails(): void {
+    const id = this.route.snapshot.paramMap.get('engineId');
+    this.ruleService.getEngineDetails(id).subscribe(engine => {
+      this.engine = engine;
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+}
