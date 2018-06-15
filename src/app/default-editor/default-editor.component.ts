f4025e94084601ee177ffda8f3e7b84d557b8e59
Drobniac <drobniac@gmail.com>
2018-06-15 12:28:36 +0200

    Create default-editor unit

---
+++ src/app/default-editor/default-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'default-editor',
+  templateUrl: './default-editor.component.html',
+  styleUrls: ['./default-editor.component.css']
+})
+export class DefaultEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
