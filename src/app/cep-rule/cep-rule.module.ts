9c521724c18ae925d0181f43255e6fb6e378282e
Drobniac <drobniac@gmail.com>
2018-06-15 10:59:06 +0200

    Create CepRuleModule

---
+++ src/app/cep-rule/cep-rule.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CepRuleService } from './cep-rule.service';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    CepRuleService
+  ]
+})
+export class CepRuleModule {}
