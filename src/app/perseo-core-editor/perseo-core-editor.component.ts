ad405225187def7779e9acb4148ee5c0ca8ea2e9
Drobniac <drobniac@gmail.com>
2018-06-15 12:29:47 +0200

    Create perseo-core-editor unit

---
+++ src/app/perseo-core-editor/perseo-core-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'perseo-core-editor',
+  templateUrl: './perseo-core-editor.component.html',
+  styleUrls: ['./perseo-core-editor.component.css']
+})
+export class PerseoCoreEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/perseo-core-editor/perseo-core-editor.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PerseoCoreEditorComponent } from './perseo-core-editor.component';
+
+@NgModule({
+  declarations: [
+    PerseoCoreEditorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PerseoCoreEditorComponent
+  ]
+})
+export class PerseoCoreEditorModule { }
