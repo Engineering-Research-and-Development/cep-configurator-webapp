7e2c89c11d7c98fb7fb9c69a337fe44e493c1ee9
Drobniac <drobniac@gmail.com>
2018-06-15 10:53:30 +0200

    Create CepEngineEditorModule

---
+++ src/app/cep-engine-editor/cep-engine-editor.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CepEngineEditorComponent } from './cep-engine-editor.component';
+
+@NgModule({
+  declarations: [
+    CepEngineEditorComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CepEngineEditorComponent
+  ]
+})
+export class CepEngineEditorModule {}
