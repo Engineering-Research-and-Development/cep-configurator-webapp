1a582a26f7ba698de33f7c84c53d2a987680f213
Drobniac <drobniac@gmail.com>
2018-06-15 18:14:02 +0200

    Create PerseoCoreRuleCreatorComponent

---
+++ src/app/perseo-core-editor/perseo-core-rule-creator/perseo-core-rule-creator.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CepRuleService } from 'app/cep-rule/cep-rule.service';
+
+@Component({
+  selector: 'perseo-core-rule-creator',
+  templateUrl: './perseo-core-rule-creator.component.html',
+  styleUrls: ['./perseo-core-rule-creator.component.css']
+})
+export class PerseoCoreRuleCreatorComponent implements OnInit {
+
+  @Input() engineId;
+  perseoCoreRule = {
+    statement: null,
+    description: null
+  }
+
+  constructor(
+    private service: CepRuleService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  submit() {
+    this.service
+      .createRule(this.engineId, this.perseoCoreRule)
+      .subscribe();
+  }
+
+}
